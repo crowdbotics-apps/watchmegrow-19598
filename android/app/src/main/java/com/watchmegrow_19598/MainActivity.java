b885c6dd35de5088b775711de3d557453dd49d38
Crowdbotics <team@crowdbotics.com>
2020-08-16 18:07:39 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/watchmegrow_19598/MainActivity.java
@@ -0,0 +1,15 @@
+package com.watchmegrow_19598;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+    * Returns the name of the main component registered from JavaScript. This is used to schedule
+    * rendering of the component.
+    */
+    @Override
+    protected String getMainComponentName() {
+        return "watchmegrow_19598";
+    }
+}
